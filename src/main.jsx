2741de68010eb998d061385e57360c9a124ae945
linswimwilson <lindsay.wilson@gmail.com>
2026-03-08 20:31:43 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Hub from './Hub.jsx'
+import Conv1 from './conversations/conv1.jsx'
+import Conv2 from './conversations/conv2.jsx'
+import Conv3 from './conversations/conv3.jsx'
+import Conv4 from './conversations/conv4.jsx'
+import Conv5 from './conversations/conv5.jsx'
+import Conv6 from './conversations/conv6.jsx'
+import Conv7 from './conversations/conv7.jsx'
+import Conv8 from './conversations/conv8.jsx'
+import Conv9 from './conversations/conv9.jsx'
+import Conv10 from './conversations/conv10.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Hub />} />
+      <Route path="/conv1" element={<Conv1 />} />
+      <Route path="/conv2" element={<Conv2 />} />
+      <Route path="/conv3" element={<Conv3 />} />
+      <Route path="/conv4" element={<Conv4 />} />
+      <Route path="/conv5" element={<Conv5 />} />
+      <Route path="/conv6" element={<Conv6 />} />
+      <Route path="/conv7" element={<Conv7 />} />
+      <Route path="/conv8" element={<Conv8 />} />
+      <Route path="/conv9" element={<Conv9 />} />
+      <Route path="/conv10" element={<Conv10 />} />
+    </Routes>
+  </HashRouter>
+)
